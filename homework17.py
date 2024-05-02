7344e806c9b08c8c9942df38245a10c3a9d7e9a5
Артем <tiom.teomin@yandex.ru>
2024-05-02 20:56:06 +0300

    Домашняя работа №17

---
+++ homework17.py
@@ -0,0 +1,10 @@
+class Buiding:
+    def __init__(self):
+        self.numberOfFloors = 0
+        self.buildingType = '0'
+
+    def __eq__(self, other):
+        one = self.numberOfFloors == other.numberOfFloors
+        second = self.buildingType == other.buildingType
+        return one, second
+

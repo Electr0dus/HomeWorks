ca70b1691cec96fd625177b926d66db14fb7f65b
Artyom <tiom.teomin@ynadex.ru>
2024-08-18 12:15:11 +0300

    ДЗ основы FastApi

---
+++ module_16_1.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+async def main_page() -> str:
+    return 'Главная страница'
+
+@app.get('/user/admin')
+async def admin_page() -> str:
+    return 'Вы вошли как админимтратор'
+
+@app.get('/user/{user_id}')
+async def page_user(user_id: int) -> str:
+    return f'Вы вошли как пользователь № {user_id}'
+
+@app.get('/user')
+async def info_user(username: str, age: int) -> str:
+    return f'Информация о пользователе. Имя {username}, Возраст: {age}'
